c473f29e317a5b29c0a021548bcf14d59d408cc2
Joshua Curry <joshuacurry2615@gmail.com>
2021-08-09 21:04:17 -0400

    Initial Commit

---
+++ BreakingBad/BreakingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadApp.swift
+//  BreakingBad
+//
+//  Created by Joshua Curry on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
